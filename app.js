221f5cfa0467bab4087eec6cb98cd6880698684b
Thiago de Oliveira Dantas <t.dod@live.com>
2021-06-28 22:32:01 -0300

    Estudos node

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+//Rotas
+const router = express.Router();
+router.get('/', (req, res)=>{
+    res.send('Olá mundo!');
+});
+
+
+// Configurações
+const app = express();
+app.use('/', router);
+
+module.exports = app;
