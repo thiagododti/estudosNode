3847da915fe1fb522acf273487da102f2c414f2e
Thiago de Oliveira Dantas <t.dod@live.com>
2021-06-28 23:07:02 -0300

    conexões
    variáveis de ambiente, conexão com banco e porta.

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 
-app.set('port', 7777);
+require('dotenv').config({path:'variables.env'});
 
+
+// Conexão ao banco de dados
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error',(error)=>{
+    console.log("Erro: "+error.message);
+});
+
+// Seleção de porta
+app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'),()=>{
     console.log("Servidor rodando na porta "+server.address().port);
 });
+
+
